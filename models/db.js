33566ed14c491807917918bba92ebdf7ac1eb842
samuel <samuel6tesfay@gmail.com>
2021-12-12 08:26:26 +0300

    v-1.5

--- models/db.js
+++ models/db.js
@@ -5,7 +5,10 @@ const devConfig = "postgresql://" + process.env.PG_USER + ":" + process.env.PG_P
 const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
-    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
+    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
